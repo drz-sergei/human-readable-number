cd6bae2028fd6ad7ab831a5e8af6a7fd2f943d70
drz-sergei <drz.sergei@gmail.com>
2022-01-05 23:25:05 +0300

    init: create human-readable-number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let a = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    let b = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number === 0) return "zero";
+
+    for (i = 2; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            a[i * 10 + j] = b[i] + " " + a[j];
+            a[i * 10 + j] = a[i * 10 + j].trim();
+        }
+    }
+
+    for (h = 1; h < 10; h++) {
+        for (i = 0; i < 100; i++) {
+            a[h * 100 + i] = a[h] + " " + "hundred" + " " + a[i];
+            a[h * 100 + i] = a[h * 100 + i].trim();
+        }
+    }
+
+    return a[number];
+};
